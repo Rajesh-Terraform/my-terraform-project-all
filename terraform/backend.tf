0910e0e985da66ea60705bd080c2d21d69627f9c
rajeshjonnalagadda294@gmail.com <rajeshjonnalagadda294@gmail.com>
2026-08-13 19:55:34 +0530

    terraform project-all

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "dhoni-demo-terraform-bucket-123456 "
+    bucket = "dhoni-demo-terraform-bucket-123456"
     key    = "networking/hub-spoke/terraform.tfstate"
     region = "ap-south-1"
   }
