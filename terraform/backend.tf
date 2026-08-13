4afdd7fd2a2f8598efa220b3fd21e65fe2881919
rajeshjonnalagadda294@gmail.com <rajeshjonnalagadda294@gmail.com>
2026-08-13 17:01:17 +0530

    terraform project-all

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -7,6 +7,14 @@ terraform {
 }
 
 
+terraform {
+  backend "s3" {
+    bucket = "harish-gaddam-bucket123"
+    key    = "networking/hub-spoke/terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
+
 
 
 
