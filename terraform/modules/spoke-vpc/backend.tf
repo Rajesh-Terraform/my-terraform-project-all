1ad2f0616ab11eec5867285cfb987cc02fe6f00b
rajeshjonnalagadda294@gmail.com <rajeshjonnalagadda294@gmail.com>
2026-08-13 17:49:41 +0530

    terraform project-all

--- terraform/modules/spoke-vpc/backend.tf
+++ terraform/modules/spoke-vpc/backend.tf
@@ -1,7 +1,11 @@
 terraform {
   backend "s3" {
-    bucket = "harish-gaddam-bucket123"
-    key    = "spoke-vpc/terraform.tfstate"
+    bucket = "dhoni-demo-terraform-bucket-123456"
+    key    = "networking/hub-spoke/terraform.tfstate"
     region = "ap-south-1"
   }
-}  
+}
+
+  
+
+
